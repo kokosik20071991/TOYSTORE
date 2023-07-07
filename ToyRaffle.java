054e08c0b662bab784b6fb3c635b84e95dcb2bf5
Тимченко Виктория Александровна <4600-715@mfc-kursk.ru>
2023-07-07 09:57:09 +0300

    Добавление основных файлов программы

---
+++ ToyRaffle.java
@@ -0,0 +1,29 @@
+public class ToyRaffle {
+    public static void main(String[] args) {
+        ToyStore store = new ToyStore();
+
+        Toy toy1 = new Toy(1, "Пазл", 10, 20);
+        Toy toy2 = new Toy(2, "Кукла", 200, 40);
+        Toy toy3 = new Toy(3, "Игрушка антистресс", 200, 90);
+        Toy toy4 = new Toy(3, "Lego", 50, 30);
+        Toy toy5 = new Toy(3, "Мягкая игрушка", 200, 90);
+        Toy toy6 = new Toy(3, "Монополия", 10, 10);
+
+        store.addToy(toy1);
+        store.addToy(toy2);
+        store.addToy(toy3);
+        store.addToy(toy4);
+        store.addToy(toy5);
+        store.addToy(toy6);
+
+
+        store.changeToyFrequency(1, 30);
+
+        store.organizeRaffle();
+
+        Toy prizeToy = store.getPrizeToy();
+        if (prizeToy != null) {
+            System.out.println("Вы выиграли: " + prizeToy.getName());
+        }
+    }
+}
